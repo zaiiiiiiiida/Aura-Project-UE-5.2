6b95b2175b8ffcf34b640aa86d64b034b1b8c9f2
zaiiiiiiiida <121565377+zaiiiiiiiida@users.noreply.github.com>
2023-10-30 16:09:19 +0600

    ClampingAttributesSettedProperly

--- Aura/Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
+++ Aura/Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -78,6 +78,14 @@ void UAuraAttributeSet::PostGameplayEffectExecute(const FGameplayEffectModCallba
 
 	FEffectProperties Props;
 	SetEffectProperties(Data, Props);
+	if(Data.EvaluatedData.Attribute == GetHealthAttribute())
+	{
+		SetHealth((FMath::Clamp(GetHealth(),0.f, GetMaxHealth())));
+	}
+	if(Data.EvaluatedData.Attribute == GetManaAttribute())
+	{
+		SetMana((FMath::Clamp(GetMana(),0.f, GetMaxMana())));
+	}
 }
 
 

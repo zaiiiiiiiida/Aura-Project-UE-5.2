1084eadea0946b3b7f6a3e75de4cc00608262616
zaiiiiiiiida <121565377+zaiiiiiiiida@users.noreply.github.com>
2023-11-16 20:47:01 +0600

    AttributeMenuRespondingToChanges

--- Aura/Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
+++ Aura/Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -8,23 +8,40 @@
 #include "AbilitySystem/AuraAttributeSet.h"
 #include "AuraGameplayTags.h"
 
-
 void UAttributeMenuWidgetController::BindCallbacksToDependencies()
 {
-	
+	UAuraAttributeSet* AS = CastChecked<UAuraAttributeSet>(AttributeSet);
+	check(AttributeInfo);
+	for (auto& Pair : AS->TagsToAttributes)
+	{
+		AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(Pair.Value()).AddLambda(
+		[this, Pair](const FOnAttributeChangeData& Data)
+		{
+			BroadcastAttributeInfo(Pair.Key, Pair.Value());
+		}
+	);
+	}
 }
-
 void UAttributeMenuWidgetController::BroadcastInitialValues()
 {
 	UAuraAttributeSet* AS = CastChecked<UAuraAttributeSet>(AttributeSet);
+	check(AttributeInfo);
 
-	check(AttributeInfo)
-for(auto& Pair: AS->TagsToAttributes)
+	for (auto& Pair : AS->TagsToAttributes)
+	{
+		BroadcastAttributeInfo(Pair.Key, Pair.Value());
+	}
+
+}
+
+void UAttributeMenuWidgetController::BroadcastAttributeInfo(const FGameplayTag& AttributeTag,
+	const FGameplayAttribute& Attribute) const
 {
-	FAuraAttributeInfo Info = AttributeInfo->FindAttributeInfoForTag(Pair.Key);
-	Info.AttributeValue =  Pair.Value().GetNumericValue(AS);
+	FAuraAttributeInfo Info = AttributeInfo->FindAttributeInfoForTag(AttributeTag);
+	Info.AttributeValue = Attribute.GetNumericValue(AttributeSet);
 	AttributeInfoDelegate.Broadcast(Info);
+}
 
-}
-}
+
+
 
--- Aura/Source/Aura/Public/UI/WidgetController/AttributeMenuWidgetController.h
+++ Aura/Source/Aura/Public/UI/WidgetController/AttributeMenuWidgetController.h
@@ -32,4 +32,9 @@ protected:
 	UPROPERTY(EditDefaultsOnly)
 	TObjectPtr<UAttributeInfo> AttributeInfo;
 	
+private:
+
+	void BroadcastAttributeInfo(const FGameplayTag& AttributeTag, const FGameplayAttribute& Attribute) const;
+
+	
 };

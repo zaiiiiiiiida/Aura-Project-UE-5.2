e4cbb5ab72cc2d44278f1ad891fc92f0d96363f0
zaiiiiiiiida <121565377+zaiiiiiiiida@users.noreply.github.com>
2023-11-21 19:35:22 +0600

    NewCollisionObjectCreated::ECC_Projectile is setted

--- Aura/Source/Aura/Private/Character/AuraCharacterBaseClass.cpp
+++ Aura/Source/Aura/Private/Character/AuraCharacterBaseClass.cpp
@@ -4,6 +4,7 @@
 #include "Character/AuraCharacterBaseClass.h"
 #include  "AbilitySystemComponent.h"
 #include "AbilitySystem/AuraAbilitySystemComponent.h"
+#include "Aura/Aura.h"
 #include "Components/CapsuleComponent.h"
 
 
@@ -14,9 +15,12 @@ AAuraCharacterBaseClass::AAuraCharacterBaseClass()
 
 	GetCapsuleComponent()->SetCollisionResponseToChannel(ECC_Camera, ECR_Ignore);
 	GetMesh()->SetCollisionResponseToChannel(ECC_Camera, ECR_Ignore);
+	GetMesh()->SetCollisionResponseToChannel(ECC_Projectile,ECR_Overlap);
+	GetMesh()->SetGenerateOverlapEvents(true);
 	Weapon=CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
 	Weapon->SetupAttachment(GetMesh(),FName("WeaponHandSocket"));
 	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	
 }
 
 

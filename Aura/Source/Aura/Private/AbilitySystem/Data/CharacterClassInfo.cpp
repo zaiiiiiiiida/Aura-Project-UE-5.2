e0792a476bdf0f7efcd0ea32ba97be0ec38c4617
zaiiiiiiiida <121565377+zaiiiiiiiida@users.noreply.github.com>
2023-11-22 16:47:07 +0600

    AttributesForEnemies

---
+++ Aura/Source/Aura/Private/AbilitySystem/Data/CharacterClassInfo.cpp
@@ -0,0 +1,9 @@
+// All the rights reserved.
+
+
+#include "AbilitySystem/Data/CharacterClassInfo.h"
+
+FCharacterClassDefaultInfo UCharacterClassInfo::GetClassDefaultInfo(ECharacterClass CharacterClass)
+{
+	return CharacterClassInformation.FindChecked(CharacterClass);
+}
---
+++ Aura/Source/Aura/Public/AbilitySystem/Data/CharacterClassInfo.h
@@ -0,0 +1,51 @@
+// All the rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "CharacterClassInfo.generated.h"
+
+class UGameplayEffect;
+
+UENUM(BlueprintType)
+enum class ECharacterClass : uint8
+{
+	Elementalist,
+	Warrior,
+	Ranger
+};
+
+USTRUCT()
+struct FCharacterClassDefaultInfo
+{
+	GENERATED_BODY()
+
+
+	UPROPERTY(EditDefaultsOnly, Category="ClassDefaults")
+	TSubclassOf<UGameplayEffect>PrimaryAttributes;
+	
+};
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UCharacterClassInfo : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+
+	UPROPERTY(EditDefaultsOnly, Category="Character Class Defaults")
+	TMap<ECharacterClass,FCharacterClassDefaultInfo>CharacterClassInformation;
+	
+	
+	UPROPERTY(EditDefaultsOnly, Category="Common Class Defaults")
+	TSubclassOf<UGameplayEffect>SecondaryAttributes;
+
+	UPROPERTY(EditDefaultsOnly, Category="Common Class Defaults")
+	TSubclassOf<UGameplayEffect>VitalAttributes;
+
+	FCharacterClassDefaultInfo GetClassDefaultInfo(ECharacterClass CharacterClass);
+	
+};
